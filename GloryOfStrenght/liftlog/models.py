dc895bb45f5ca6906cc2f16e071f04fdc8576144
Oskar Swat <oskar.swat@tlen.pl>
2025-05-13 01:29:47 +0200

    Model create

--- GloryOfStrenght/liftlog/models.py
+++ GloryOfStrenght/liftlog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class LiftVideo(models.Model):
+    LIFT_CHOICES = [
+        ('SQ', 'Squat'),
+        ('BP', 'Bench press'),
+        ('DL', 'Deadlift'),
+    ]
+
+    title = models.CharField(max_length=100)
+    lift_type = models.CharField(max_length=2, choices=LIFT_CHOICES)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    video = models.FileField(upload_to='videos/')
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.get_lift_type_display()}"
+    
